9e5570985f1503a604850636475fe74de9966f80
amberclaw <k545v8d2w4hn@kimo.com>
2023-01-24 12:33:08 +0800

    商品功能 - 實作查詢商品列表-基礎實作

--- src/main/java/com/bill/springbootmall/dao/ProductDao.java
+++ src/main/java/com/bill/springbootmall/dao/ProductDao.java
@@ -3,8 +3,11 @@ package com.bill.springbootmall.dao;
 import com.bill.springbootmall.dto.ProductRrquest;
 import com.bill.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
 
+    List<Product> getProducts();
     Product getProductById(Integer prroductId);
     Integer createProduct(ProductRrquest productRrquest);
     void updateProduct(Integer productId, ProductRrquest productRrquest);
--- src/main/java/com/bill/springbootmall/service/ProductService.java
+++ src/main/java/com/bill/springbootmall/service/ProductService.java
@@ -2,9 +2,11 @@ package com.bill.springbootmall.service;
 
 import com.bill.springbootmall.dto.ProductRrquest;
 import com.bill.springbootmall.model.Product;
+import java.util.List;
 
 public interface ProductService {
 
+    List<Product> getProducts();
     Product getProductById(Integer prroductId);
     Integer createProduct(ProductRrquest productRrquest);
     void updateProduct(Integer productId, ProductRrquest productRrquest);
